8274e3ad63539cbdb59051773fd991568f2c2342
arifudin <arifudin@bni.co.id>
2024-03-22 13:15:44 +0700

    first commit crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configurations from './config/configurations';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: configurations().database.host,
+        port: configurations().database.port,
+        username: configurations().database.username,
+        database: configurations().database.database,
+        password: configurations().database.password,
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configurations.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  database: {
+    type: 'postgres',
+    database: process.env.DATABASE_NAME,
+    port: parseInt(process.env.DATABASE_PORT),
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  },
+});
